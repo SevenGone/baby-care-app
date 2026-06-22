345116e074f79e49f220242717b18eb1e229ab44
SevenGone <sevengone@SevenGonedeMacBook-Air.local>
2026-06-22 14:25:07 +0800

    Fix Material theme dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation("androidx.compose.foundation:foundation:1.7.1")
     implementation("androidx.compose.material3:material3:1.3.0")
     implementation("androidx.compose.material:material-icons-extended:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
